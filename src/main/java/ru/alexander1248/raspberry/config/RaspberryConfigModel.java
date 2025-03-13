c74d60db098ae17edd8335e4fac7c012764ba411
Alexander <sasha.izmyloff@mail.ru>
2025-03-14 01:29:30 +0300

    Localization

--- src/main/java/ru/alexander1248/raspberry/config/RaspberryConfigModel.java
+++ src/main/java/ru/alexander1248/raspberry/config/RaspberryConfigModel.java
@@ -2,11 +2,10 @@ package ru.alexander1248.raspberry.config;
 
 import io.wispforest.owo.config.annotation.Config;
 import io.wispforest.owo.config.annotation.Modmenu;
-import ru.alexander1248.raspberry.Raspberry;
 
 import java.util.concurrent.TimeUnit;
 
-@Modmenu(modId = Raspberry.MOD_ID)
+@Modmenu(modId = "raspberry")
 @Config(name = "raspberry-config", wrapperName = "RaspberryConfig")
 public class RaspberryConfigModel {
     public String modListUri = "https://raw.githubusercontent.com/Alexander1248/RaspberryMod/refs/heads/main/data/index.json";
