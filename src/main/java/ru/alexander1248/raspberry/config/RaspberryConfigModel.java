fe7dc2d14eade8783006b0a6af4811c240f3f57b
Alexander <sasha.izmyloff@mail.ru>
2025-03-14 19:16:35 +0300

    Organization Fix

--- src/main/java/ru/alexander1248/raspberry/config/RaspberryConfigModel.java
+++ src/main/java/ru/alexander1248/raspberry/config/RaspberryConfigModel.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 @Modmenu(modId = "raspberry")
 @Config(name = "raspberry-config", wrapperName = "RaspberryConfig")
 public class RaspberryConfigModel {
-    public String modListUri = "https://raw.githubusercontent.com/Alexander1248/RaspberryMod/refs/heads/main/data/index.json";
+    public String modListUri = "https://raw.githubusercontent.com/Raspberry-Pi-Craft/RaspberryMod/refs/heads/main/data/index.json";
     public long connectionTimeout = 10;
     public TimeUnit connectionTimeoutUnit = TimeUnit.SECONDS;
     public int connectionRetry = 3;
